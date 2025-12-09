a05df2c7d6e3c91725fb8a3c37c21bf3c1c1129b
unknown <stefanstaxbusiness@gmail.com>
2025-12-09 16:18:09 +0100

    Graph wip

--- s10/city.php
+++ s10/city.php
@@ -20,14 +20,123 @@ if (!empty($city)) {
     }
 
     if (!empty($filename)) {
-        $data = json_decode(
+        $results = json_decode(
             file_get_contents("compress.bzip2://" . __DIR__ . "/data/{$filename}"),
             true
-        );
+        )["results"];
 
-        var_dump($data);
+        $stats = [];
+        $units = [
+            "pm25" => null,
+            "pm10" => null
+        ];
+
+
+        foreach ($results as $result) {
+            if (!empty($units["pm25"]) && !empty($units["pm10"])) break;
+
+            if ($result["parameter"] === "pm25") {
+                $units["pm25"] = $result["unit"];
+            }
+
+            if ($result["parameter"] === "pm10") {
+                $units["pm10"] = $result["unit"];
+            }
+        }
+
+
+        foreach ($results as $result) {
+            if ($result["parameter"] !== "pm25" && $result["parameter"] !== "pm10") continue;
+            if ($result["value"] < 0) continue;
+            $month = substr($result["date"]["local"], 0, 7);
+            if (!isset($stats[$month])) {
+                $stats[$month] = [
+                    "pm25" => [],
+                    "pm10" => []
+                ];
+            }
+            $stats[$month][$result["parameter"]][] = $result["value"];
+        }
     }
 }
 
+function showMonthIcon($value)
+{
+    $getMonth = substr($value, 5, 7);
+
+    if ($getMonth !== "10") {
+        $getMonth = str_replace("0", "", $getMonth);
+    }
+
+    $spring = [3, 4, 5];
+    $summer = [6, 7, 8];
+    $autumn = [9, 10, 11];
+
+    if (in_array($getMonth, $spring)) {
+        echo "☔";
+    } else if (in_array($getMonth, $summer)) {
+        echo "🩴";
+    } else if (in_array($getMonth, $autumn)) {
+        echo "🧥";
+    } else {
+        echo "☃️";
+    }
+}
+
+if (!empty($city)) { ?>
+    <div style="padding: 0.675em; border-radius: 1.25em; margin-bottom: 1em; background-color: #90D5FF; color: white;">
+        <h1 style="font-size: 1.5rem; font-weight: bold;">AQI Weather Data for <?php echo $city; ?></h1>
+    </div>
+
+    <canvas id="aqi-chart" width="400" height="400"></canvas>
+    <script>
+        document.addEventListener("DOMContentLoaded", function() {
+            const ctx = document.getElementById('aqi-chart');
+            ctx.getContext("2d");
+            const aqiChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+                    datasets: [{
+                        label: '# of Votes',
+                        data: [12, 19, 3, 5, 2, 3],
+                        backgroundColor: '#5152fb20',
+                        borderColor: '#5152fb',
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        y: {
+                            beginAtZero: true
+                        }
+                    }
+                }
+            });
+        })
+    </script>
+    <table>
+        <thead>
+            <tr>
+                <th>Month</th>
+                <th>PM 2.5 Concentration</th>
+                <th>PM 10 Concentration</th>
+            </tr>
+        </thead>
+        <tbody>
+            <?php foreach ($stats as $month => $measurements) { ?>
+                <tr>
+                    <th><?php echo showMonthIcon($month) ?><?php echo secureText($month) ?></th>
+                    <?php if (count($measurements["pm25"]) > 0) { ?>
+                        <td><?php echo secureText(array_sum($measurements["pm25"]) / count($measurements["pm25"])) ?> <?php echo $units["pm25"] ?></td>
+                    <?php } ?>
+                    <?php if (count($measurements["pm10"]) > 0) { ?>
+                        <td><?php echo secureText(array_sum($measurements["pm10"]) / count($measurements["pm10"])) ?> <?php echo $units["pm10"] ?></td>
+                    <?php } ?>
+                </tr>
+            <?php } ?>
+        </tbody>
+    </table>
+<?php }
 
 require __DIR__ . "/views/footer.inc.php";
--- s10/views/header.inc.php
+++ s10/views/header.inc.php
@@ -5,6 +5,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Air Quality App</title>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.5.0/chart.umd.js" integrity="sha512-D4pL3vNgjkHR/qq+nZywuS6Hg1gwR+UzrdBW6Yg8l26revKyQHMgPq9CLJ2+HHalepS+NuGw1ayCCsGXu9JCXA==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
 </head>
 
 <body>
