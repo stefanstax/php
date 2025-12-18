8ed2b82fef1bdc7a5420f2e0368eca7bfa79f45b
unknown <stefanstaxbusiness@gmail.com>
2025-12-18 09:00:58 +0100

    S14 wip

--- s14/index.php
+++ s14/index.php
@@ -9,6 +9,9 @@ $pdo = new PDO("mysql:host=localhost;dbname=course", "root", "", [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
 ]);
 
+$id = (int)$_GET["id"];
+
+$query = "SELECT * FROM `notes` WHERE `id` = {$id}";
 $statement = $pdo->prepare("SELECT id, email FROM users ORDER BY email ASC");
 $statement->execute();
 $results = $statement->fetchAll(PDO::FETCH_ASSOC);
